85e4dff177eebc8e23bd126e3417b2bca596200d
Yash Thakkar <yashthakkar6254@gmail.com>
2022-03-19 22:15:36 +0530

    Yash Thakkar 19BCE10388 Final

--- src/app.js
+++ src/app.js
@@ -2,23 +2,23 @@ const PLAYERS = [
     "Spiderman",
     "Captain America",
     "Wonderwoman",
-    // "Popcorn",
-    // "Gemwoman",
-    // "Bolt",
-    // "Antwoman",
-    // "Mask",
-    // "Tiger",
-    // "Captain",
-    // "Catwoman",
-    // "Fish",
-    // "Hulk",
-    // "Ninja",
-    // "Black Cat",
-    // "Volverine",
-    // "Thor",
-    // "Slayer",
-    // "Vader",
-    // "Slingo"
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
 ];
 
 // Player Class
@@ -26,6 +26,9 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
+        this.id = id + 1;
+        this.name = name;
+        this.type = type;
 
     }
 
@@ -38,7 +41,23 @@ class Player {
     view = () => {
         // Accumulate HTML template
         // Type your code here
+        let player = document.createElement("div")
+        player.setAttribute("class", "player")
+        player.setAttribute("data-id", this.id)
 
+        let image = document.createElement("img")
+        image.src = "images/super-" + this.id + ".png"
+
+
+        let name = document.createElement("div")
+        name.setAttribute("class", "name")
+        name.innerHTML = this.name
+
+        let strength = document.createElement("div")
+        strength.setAttribute("class", "strength")
+        strength.innerHTML = this.getRandomStrength()
+
+        player.append(image, name, strength)
     
         return player;
     }
@@ -50,21 +69,23 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
-
+    let role = 'villain'
+        this.players = players.map((player, index) => {
+            role = role === 'hero' ? 'villain' : 'hero'
+            return new Player(index, player, role)
+        })
     }
 
     // Display players in HTML
     viewPlayers = () => {
         let team = document.getElementById('heroes');
         team.innerHTML = '';
-        let fragment =
-            this.buildPlayers('hero');
+        let fragment =this.buildPlayers('hero');
         team.append(fragment);
 
         team = document.getElementById('villains');
         team.innerHTML = '';
-        fragment =
-            this.buildPlayers('villain');
+        fragment =this.buildPlayers('villain');
         team.append(fragment);
     }
 
@@ -74,6 +95,7 @@ class Superwar {
         this.players
             .filter(player => player.type == type)
             .forEach(player => fragment.append(player.view()));
+        this.players.filter(player => player.type == type).forEach(player => fragment.append(player.view()));
         return fragment;
     }
 
